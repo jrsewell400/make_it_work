35de7069a4c4d500428fda9ed8a434287e3dde45
Jordan Sewell <jrsewell400@gmail.com>
2020-02-07 11:23:27 -0700

    Finished US2

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -3,36 +3,49 @@ require 'rails_helper'
 RSpec.describe "As a visitor,", type: :feature do
   describe "When I visit the contestants index page" do
     it "I see a list of names of all the contestants and projects that they've been on" do
+        challenge_1 = Challenge.create!(theme: "Apartment Furnishings",
+                                        project_budget: 150)
 
-        contestant_1 = Contestant.create!(name: "Kentaro Kameyama",
-                                          age: 30,
-                                          hometown: "Nowhere, IN",
-                                          years_of_experience: 1,
-                                          project_id: contestant_1.id)
+        kentaro = Contestant.create!(name: "Kentaro Kameyama",
+                                    age: 30,
+                                    hometown: "Nowhere, IN",
+                                    years_of_experience: 1,
+                                    )
         
-        contestant_2 = Contestant.create!(name: "Jay McCarroll",
-                                          age: 30,
-                                          hometown: "Everywhere, CA",
-                                          years_of_experience: 7,
-                                          project_id: contestant_2.id)
+        jay = Contestant.create!(name: "Jay McCarroll",
+                                age: 30,
+                                hometown: "Everywhere, CA",
+                                years_of_experience: 7,
+                                )
 
         project_1 = Project.create!(name: "Litfit",
                                     material: "Lamp Shade",
-                                   )
+                                    challenge_id: challenge_1.id)
 
         project_2 = Project.create!(name: "LeatherFeather",
                                     material: "Leather",
-                                   )  
+                                    challenge_id: challenge_1.id)
 
         project_3 = Project.create!(name: "Rug Tuxedo",
                                     material: "Rugs",
-                                   )                               
-        binding.pry
-        # visit "/projects/#{project_1.id}"
-        
-        # expect(page).to have_content("Challenge Theme: Apartment Furnishings")
-        # expect(page).to have_content("Litfit")
-        # expect(page).to have_content("Lamp Shade")
+                                    challenge_id: challenge_1.id)  
+                                    
+        kentaro_projects1 = ContestantProject.create(contestant_id: kentaro.id,
+                                                     project_id: project_1.id)
+
+        kentaro_projects2 = ContestantProject.create(contestant_id: kentaro.id,
+                                                     project_id: project_3.id)
+
+        jay_projects1 = ContestantProject.create(contestant_id: jay.id,
+                                                project_id: project_2.id)
+
+        visit "/contestants"
+ 
+        expect(page).to have_content(kentaro.name)
+        expect(page).to have_content(jay.name)
+        expect(page).to have_content(project_1.name)
+        expect(page).to have_content(project_2.name)
+        expect(page).to have_content(project_3.name)
     end
   end
 end
